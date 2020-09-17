a9d66b47937200323cc2623ed06ee3a097a20c2c
Steinunntry <steinunntryggva@gmail.com>
2020-09-17 18:50:15 +0000

    first commit

---
+++ tiletraveller.py
@@ -0,0 +1,19 @@
+
+
+# Hér munum við gera forritið Tile traveller sem er ferðalag frá reit 1.1 til 3.1
+
+
+directions = input("Direction:")
+#föll : 
+def where_to_next():
+    # með þessu falli tökum við inn núverandi staðsetningu og sláum inn hvert við viljum fara næst.
+    #
+    # reitur 1.1 ------ N
+    #reitur 1.2 ------- S / A / N       Skrifa hér hvaða ferða möguleikar eru í hverjum reit
+    #reitur 1.3 ------- S / A           
+    #reitur 2.1 ------- N
+    #reitur 2.2 ------- V / S
+    #reitur 2.3 ------- V / A
+    #reitur 3.1 ------- Victory
+    #reitur 3.2 ------- S / N
+    #reitur 3.3 ------- V / N
